6be773cb367dfa1fcf2be1e1172da9aee607a72a
Attila <atszamaster@gmail.com>
2017-11-21 02:14:15 +0100

    Gamelogic component added.

---
+++ angularclient/src/app/components/gamelogic/gamelogic.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-gamelogic',
+  templateUrl: './gamelogic.component.html',
+  styleUrls: ['./gamelogic.component.css']
+})
+export class GamelogicComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
